fc375193b4292c3b686d173b82d93323b60c2485
Houssam Bourkane <hbourkan@e2r9p7.1337.ma>
2023-02-22 17:42:42 +0100

    wip

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,34 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthDto } from './dto';
-import * as argon from 'argon2';
-import { PrismaClientUnknownRequestError } from '@prisma/client/runtime';
+import axios from 'axios';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+    constructor (private prisma: PrismaService) {}
 
-  async signup(b: AuthDto) {
-    const hash = await argon.hash(b.password);
-    try {
-      const user = await this.prisma.User.create({
-        data: {
-          email: b.email,
-          password: hash,
-        },
-        // select: {
-        //   id: true,
-        //   email: true,
-        // },
-      });
-      delete user.password;
-      return user;
-    } catch (error) {
-      if(error instanceof PrismaClientUnknownRequestError)
-         
+
+
+    async fetch_data(code : string) {
+        const client_id = "u-s4t2ud-8dac923a7faa5e271e7a48b1823cd099d6b981eb5193dca17613e24b4cd72ca5";
+        const client_secret = "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";
+        const redirect_uri = "http://localhost:3000/auth";
+        const token = await axios.post('https://api.intra.42.fr/oauth/token', {
+            "grant_type":"authorization_code",
+            client_id,
+            client_secret,
+            code,
+            redirect_uri
+        })
+        const data = (await axios.get('https://api.intra.42.fr/v2/me', {
+            headers: {
+                'Authorization': `Bearer ${token.data.access_token}`
+            }
+        })).data
+
+        const user_id : string = data.id;
+        const user_email : string = data.email;
+        const user_avatar : string = data.image.link;
+        const upsertUser = await this.prisma.user.upsert({
+            where: {
+              this.prisma.user.avatar_id: '#id',
+            },
+            update: {
+              email: 'newmail@prisma.io',
+            },
+            create: {
+              email: 'viola@prisma.io',
+              avatar_id: 'Viola the Magnificent',
+            },
+          })
+        return data;
     }
-  }
-  signin() {
-    return `s`;
-  }
 }
