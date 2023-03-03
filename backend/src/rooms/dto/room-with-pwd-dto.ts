655321b991ca57b913d70fc38c9bf2b19c55d407
houssbrk <houssambourkane@gmail.com>
2023-03-03 17:02:19 +0100

    fixed some validations

--- backend/src/rooms/dto/room-with-pwd-dto.ts
+++ backend/src/rooms/dto/room-with-pwd-dto.ts
@@ -1,10 +1,16 @@
-import { IsOptional, IsString, Length } from 'class-validator';
+import {
+  IsOptional,
+  IsString,
+  Length,
+  ValidateIf,
+} from 'class-validator';
 
 export class RoomWithPwd {
+  @ValidateIf((e) => e.type === 'protected')
   @IsOptional()
   @IsString()
   @Length(8, 20, {
     message: 'Password must be between 8 and 20 characters',
   })
-  password?: string;
+  password: string;
 }
