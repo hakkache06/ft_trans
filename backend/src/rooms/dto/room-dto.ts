064aefe3ea03cb4cd290ce4f1cd8a9f494654f99
Abdessamad <ael-bour@student.1337.ma>
2023-03-03 15:18:06 +0100

    Fix

--- backend/src/rooms/dto/room-dto.ts
+++ backend/src/rooms/dto/room-dto.ts
@@ -1,5 +1,12 @@
 import { RoomType } from '@prisma/client';
-import { IsEnum, IsNotEmpty, IsOptional, Length } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  ValidateIf,
+} from 'class-validator';
 
 export class RoomDto {
   @Length(3, 20, {
@@ -12,7 +19,9 @@ export class RoomDto {
   @IsEnum(RoomType)
   type: RoomType;
 
+  @ValidateIf((e) => e.type === 'protected')
   @IsOptional()
+  @IsString()
   @Length(8, 20, {
     message: 'Password must be between 8 and 20 characters',
   })
